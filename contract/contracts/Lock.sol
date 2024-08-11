0ff933dbe449f484a657f751c6b729b52accba29
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2024-08-11 06:04:55 -0800

    almost contract is done

--- contract/contracts/Lock.sol
+++ contract/contracts/Lock.sol
@@ -1,34 +1,71 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+contract Tracking_Shipment{
+    enum ShipmentStatus{PENDING, IN_TRANSIT, DELIVERED}
+    struct Shipment{
+        address sender;
+        address receiver;
+        uint256 pickupTime;
+        uint256 deliveryTime;
+        uint256 distance;
+        uint256 price;
+        ShipmentStatus status;
+        bool paid;
+    }
+    mapping(address => Shipment[]) public shipments;
+    uint256 public shipmentCounts;
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+    struct TypeShipment{
+        address sender;
+        address receiver;
+        uint256 deliveryTime;
+        uint256 pickupTime;
+        uint256 distance;
+        uint256 price;
+        ShipmentStatus status;
+        bool paid;
+    }
+    TypeShipment[] public typeShipments;
 
-    event Withdrawal(uint amount, uint when);
+    event ShipmentCreated(address indexed sender, address indexed receiver, uint256 pickupTime, uint256 distance, uint256 price);
+    event ShipmentInTransit(address indexed sender, address indexed receiver, uint256 pickupTime);
+    event ShipmentInDelivered(address indexed sender, address indexed receiver, uint256 deliveredTime);
+    event ShipmentPaid(address indexed sender, address indexed receiver, uint256 price);
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    constructor(){
+        shipmentCounts=0;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    function createdShipment(address _receiver, uint256 _pickupTime, uint256 _distance, uint256 _price) public payable{
+       require(msg.value == _price, "Payment amount must be match the price");
+       Shipment memory shipment = Shipment(msg.sender, _receiver, _pickupTime, 0,
+       _distance, _price, ShipmentStatus.PENDING, false);
+      
+      shipments[msg.sender].push(shipment);
+      shipmentCounts++;
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+      TypeShipment(
+        msg.sender,
+        _receiver,
+        _pickupTime,
+        0,
+        _distance,
+        _price,
+        ShipmentStatus.PENDING,
+        false
+      );
+      emit ShipmentCreated(msg.sender, _receiver, _pickupTime, _distance, _price);
+    } 
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    function StartShipment(address _sender, address _receiver, uint256 _index) public {
+       Shipment storage Shipment = shipments[_sender][_index];
+       TypeShipment storage typeShipment= typeShipments[_index];
+       require(Shipment.receiver == _receiver, "Invalid address");
+       require(Shipment.status == ShipmentStatus.PENDING, "Shipment already in transit");
 
-        owner.transfer(address(this).balance);
+       Shipment.status= ShipmentStatus.IN_TRANSIT;
+       typeShipment.status = ShipmentStatus.IN_TRANSIT;
+       emit  ShipmentInTransit(_sender, _receiver, Shipment.pickupTime);
     }
 }
